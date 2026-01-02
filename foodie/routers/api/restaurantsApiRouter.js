1243836af71e403875474e13766baf21563a1ddb
roshan200616 <roshanroshan1612@gmail.com>
2026-01-02 19:37:47 +0530

    curd compeleted

--- foodie/routers/api/restaurantsApiRouter.js
+++ foodie/routers/api/restaurantsApiRouter.js
@@ -4,25 +4,119 @@ import express from "express";
 import queryExec from "../../DBconnection.js"
 const router = express.Router()
 
-router.get('/',async (req,res)=>{
-   try{
-    const result = await queryExec(`select * from restaurants`)
-    if(result.length === 0){
-        return res.status(404).json({message : "not found" })
+router.get('/', async (req, res) => {
+    try {
+        const result = await queryExec(`select 
+            restaurant_id,
+            name,
+            email,
+            phone,
+            address,
+            owner_name,
+            cuisine_type,
+            is_open
+             from restaurants`)
+        if (result.length === 0) {
+            return res.status(404).json({ message: "Not found" })
+        }
+        else {
+            console.log(result)
+            return res.status(200).json(result )
+            
+        }
     }
-    else{
-        return res.status(200).json({result})
-    }
-   }
-   catch(err){
+    catch (err) {
         console.log(err.message)
         res.send(err.message)
-   }
+    }
 })
-router.post('/',async(req,res)=>{
-   try{
-    const data= req.body;
-    console.log(data)
-   }
+router.get("/:id",async(req,res)=>{
+    try{
+        const id = req.params.id
+        const result = await queryExec(`select 
+            restaurant_id,
+            name,
+            email,
+            phone,
+            address,
+            owner_name,
+            cuisine_type,
+            is_open
+             from restaurants where restaurant_id = ? `,[id])
+             if(result.length === 0){
+                res.status(404).json({message:'Not found the restaurant '})
+             }
+             else{
+                res.status(200).json(result)
+             }
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json(err.message)
+    }
+
 })
+router.post('/', async (req, res) => {
+    try {
+        const { name, owner_name, email, phone, address, cuisine_type, password } = req.body;
+        if (!name || !owner_name || !email || !phone || !address || !cuisine_type || !password) {
+            return res.status(400).json({ message: "All fields are required" })
+        }
+        const values = [name, email, phone, address, owner_name, cuisine_type, password]
+        const result = await queryExec(`insert into restaurants(name,email,phone,address,owner_name,cuisine_type,password)
+        values(?,?,?,?,?,?,?)`, values
+
+        )
+        if (result.affectedRows === 0) {
+            return res.status(400).json({
+                message: "Bad request"
+            })
+        }
+        else {
+            return res.status(201).json({ message: "Restaurant register successfully " })
+        }
+    }
+
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: err.message })
+    }
+})
+router.put('/:id',async(req,res)=>{
+    try{
+        const id = req.params.id
+        const data = req.body
+        const keys = Object.keys(data)
+        const values = Object.values(data)
+        const set = keys.map(key=>`${key}=?`).join(',')
+        const result = await queryExec(`update  restaurants set ${set} where restaurant_id = ? `,[...values,id])
+        if(result.affectedRows === 0 ){
+            return res.status(404).json({message : 'not found the restaurant'})
+        }
+        else{
+            return res.status(200).json({message:'Updated successfully'})
+        }
+    }
+    catch(err){ 
+        console.log(err)
+        res.status(500).json({message:err.message})
+    }
+})
+router.delete('/:id',async(req,res)=>{
+    try{
+    const id = req.params.id 
+    const result = await queryExec(`delete from restaurants where restaurant_id = ?`,[id])
+    if(result.affectedRows === 0){
+        return res.status(404).json({message : 'Not found restaurant'})
+    }
+    else{
+        return res.status(200).json({message : 'Deleted successfully'})
+    }
+}
+catch(err){
+    console.log(err)
+    return res.status(500).json({message : err.message})
+}
+})
+
 export default router
--- foodie/routers/ui/restaurantsUiRouter.js
+++ foodie/routers/ui/restaurantsUiRouter.js
@@ -4,8 +4,7 @@ const router = express.Router()
 
 router.get("/", async (req, res) => {
     try {
-        const page = "dashboard"
-        res.render("pages/restaurantHome.ejs",{page})
+        res.render("pages/restaurant/restaurantHome.ejs")
     }
     catch (err) {
         console.log(err.message)
@@ -14,7 +13,7 @@ router.get("/", async (req, res) => {
 router.get("/menu",async(req,res)=>{
     try{
         const page = "menu"
-        res.render('pages/restaurantMenu.ejs',{page})
+        res.render('pages/restaurant/restaurantMenu.ejs',{page})
     }
     catch(err){
         console.log(err.message)
@@ -23,10 +22,38 @@ router.get("/menu",async(req,res)=>{
 router.get("/menu/add",async(req,res)=>{
     try{
         const page = 'add'
-        res.render('pages/menuAdd.ejs',{page})
+        res.render('pages/restaurant/menuAdd.ejs',{page})
     }
     catch(err){
         console.log(err.message)
     }
 })
+router.get("/dashborad",(req,res)=>{
+     try{
+         const page='dashborad'
+          res.render("pages/restaurant/restaurantDashborad.ejs",{page})
+    }
+    catch(err){
+         console.log(err.message)
+    }
+})
+router.get("/register",(req,res)=>{
+    try{
+          res.render("pages/restaurant/restaurantRegister.ejs")
+    }
+    catch(err){
+         console.log(err.message)
+    }
+})
+router.get("/login",(req,res)=>{
+    try{
+         res.render("pages/restaurant/restaurantLogin.ejs")
+
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+
+
 export default router
